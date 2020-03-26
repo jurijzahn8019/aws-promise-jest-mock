d511c2b7edada731ff02a72c824ec8bcda75b1e7
Jurij Zahn <jurij.zahn@otto.de>
2020-03-26 09:57:33 +0100

    chore(ci): migrate from travis to github actions (#158)
    * chore(ci): migrate from travis to github actions
    
    * remove travic config
    
    * remove npmrc

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,22 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import excludeDependenciesFromBundle from "rollup-plugin-exclude-dependencies-from-bundle";
 
 export default {
   input: "./src/index.ts",
-  output: { dir: "dist/", format: "cjs" },
-  plugins: [typescript({ tsconfig: "tsconfig.prod.json" })],
-  external: ["aws-sdk", "crypto"]
+  output: [
+    { file: "./dist/index.cjs.js", format: "cjs" },
+    { file: "./dist/index.es6.js", format: "es" },
+  ],
+  plugins: [
+    typescript({ tsconfig: "./tsconfig.prod.json" }),
+    resolve({ preferBuiltins: true }),
+    commonjs({ extensions: [".js", ".ts"] }),
+    excludeDependenciesFromBundle({
+      peerDependencies: true,
+      dependencies: true,
+    }),
+  ],
+  external: ["aws-sdk"],
 };
